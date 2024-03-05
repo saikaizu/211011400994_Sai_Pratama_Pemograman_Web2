feaa4eee04e151fbc2efc6127248ab589a4bf7a3
Sai Pratama <107693463+saikaizu@users.noreply.github.com>
2024-03-05 13:52:05 +0700

    Add files via upload

---
+++ pertemuan1.php
@@ -0,0 +1,7 @@
+<?php
+echo "<center><div style='font-size: 72px; color: red; text-align: justify; display: flex; justify-content: center;'>BIODATA</div></center>";
+echo "<center><div style='font-size: 36px; color: blue; text-align: justify; display: flex; justify-content: center;'>Nama&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: <b>Achmad Riki</b></div></center>";
+echo "<center><div style='font-size: 36px; color: blue; text-align: justify; display: flex; justify-content: center;'>Email&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: ariii@yahoo.con</div></center>";
+echo "<center><div style='font-size: 36px; color: blue; text-align: justify; display: flex; justify-content: center;'>Jurusan&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Teknik Informatika</div></center>";
+echo "<center><div style='font-size: 36px; color: blue; text-align: justify; display: flex; justify-content: center;'>Hobby&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;: Travelling</div></center>";
+?>
